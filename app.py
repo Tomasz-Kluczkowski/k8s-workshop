60c9150c76c3f7e700489041f79c9e66acc8e106
Tomasz-Kluczkowski <tomasz.kluczkowski@flexciton.com>
2022-08-22 17:38:32 +0100

    Add 1 route flask app :)

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return """
+    <!DOCTYPE html>
+    <head>
+        <title>Kubernetes Workshop Test App</title>
+        <style>
+            body {
+                background-color: #2D2D2D;
+            }       
+    
+            h1 {
+                color: #C26356;
+                font-size: 30px;
+                font-family: Menlo, Monaco, fixed-width;
+            }
+    
+            p {
+                color: white;
+                font-family: "Source Code Pro", Menlo, Monaco, fixed-width;
+            }       
+        </style>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <p>Can you make this run in a container?</p>
+        <p>What about in Kubernetes?</p>
+    </body>
+    </html>
+"""
