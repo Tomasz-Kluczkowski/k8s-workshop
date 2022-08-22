8dd785883ec4bce5a629043afc263f2f7c0c1252
Tomasz-Kluczkowski <tomasz.kluczkowski@flexciton.com>
2022-08-22 19:23:46 +0100

    Fill in content for final solution.

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.10.2-slim
+
+WORKDIR /app
+
+COPY requirements.txt ./
+
+RUN pip install -r requirements.txt
+
+COPY . ./
+
+EXPOSE 5000
+
+ENTRYPOINT ["flask", "--debug", "run" , "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import socket
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,31 +7,34 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    hostname = socket.gethostname()
+
     return """
     <!DOCTYPE html>
     <head>
         <title>Kubernetes Workshop Test App</title>
         <style>
-            body {
+            body {{
                 background-color: #2D2D2D;
-            }       
+            }}       
     
-            h1 {
+            h1 {{
                 color: #C26356;
                 font-size: 30px;
                 font-family: Menlo, Monaco, fixed-width;
-            }
+            }}
     
-            p {
+            p {{
                 color: white;
                 font-family: "Source Code Pro", Menlo, Monaco, fixed-width;
-            }       
+            }}       
         </style>
     </head>
     <body>
         <h1>Hello World!</h1>
         <p>Can you make this run in a container?</p>
         <p>What about in Kubernetes?</p>
+        <p>My hostname is: {0}</p>
     </body>
     </html>
-"""
+""".format(hostname)
